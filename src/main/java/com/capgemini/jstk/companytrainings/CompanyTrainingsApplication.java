c959c7e8a6adc14f845b9cf2afaa071f77545b4d
SebastianStaszalek <sebastian.staszalek@gmail.com>
2018-08-21 12:13:04 +0200

    init commit

---
+++ src/main/java/com/capgemini/jstk/companytrainings/CompanyTrainingsApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.jstk.companytrainings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyTrainingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyTrainingsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/jstk/companytrainings/CompanyTrainingsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.jstk.companytrainings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyTrainingsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
